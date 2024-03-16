5ff408ad30e3e5f52f94a572dd7f1bcee66d0bb9
Lucas Franson <lucasfranson.8@hotmail.com>
2024-03-16 18:12:01 -0500

    Created new pages and integration with backend

---
+++ Front-end/course-registration-system/src/components/CourseBlocks/TableCourseBlocks.js
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Paper from '@mui/material/Paper';
+
+
+function Row(props) {
+
+    const { block, courses } = props;
+
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <>
+            <TableRow>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    Block {block.blockId}
+                </TableCell>
+                <TableCell>{block.startDate}</TableCell>
+                <TableCell>{block.endDate}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <Box sx={{ margin: 1 }}>
+                        <Table size="small" aria-label="purchases">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Title</TableCell>
+                                    <TableCell>Description</TableCell>
+                                    <TableCell>Teacher</TableCell>
+                                    <TableCell>Format</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {courses && courses.length > 0 ? 
+                                    courses.filter(x => x.block.blockId === block.blockId).map((course) => (
+                                    <TableRow key={course.blockCourseId}>
+                                        <TableCell component="th" scope="row">
+                                            {course.title}
+                                        </TableCell>
+                                        <TableCell>{course.description}</TableCell>
+                                        <TableCell>{course.teacher?.name}</TableCell>
+                                        <TableCell>{course.campusOrDE}</TableCell>
+                                    </TableRow>
+                                )) : (
+                                    <TableRow key={123}>
+                                        <TableCell colSpan={3} align='center'>No record available</TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </Box>
+                </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    );
+}
+
+export default function TableCourseBlocks(props) {
+
+    const [blocks, setBlocks] = React.useState([]);
+
+    const { courses } = props;
+
+    React.useEffect(() => {
+
+        if (courses) {
+            let block = [];
+            courses.forEach((course) => {
+                if (course.block) {
+                    let exist = block.find(x => x.blockId === course.block.blockId);
+                    if (!exist) {
+                        let value = {
+                            blockId: course.block.blockId,
+                            endDate: course.block.endDate,
+                            startDate: course.block.startDate
+                        };
+                        block.push(value);
+                    }
+                }
+            });
+            setBlocks(block);
+        }
+
+    }, [courses]);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                <TableRow>
+                    <TableCell />
+                    <TableCell>Block ID</TableCell>
+                    <TableCell>Block Start Date</TableCell>
+                    <TableCell>Block End Date</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {blocks.map((block) => (
+                    <Row key={block.blockId} block={block} courses={courses} />
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
--- Front-end/course-registration-system/src/services/StudentService.js
+++ Front-end/course-registration-system/src/services/StudentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.create({
+export const instance = axios.create({
     baseURL: 'http://127.0.0.1:8080'
 });
 
