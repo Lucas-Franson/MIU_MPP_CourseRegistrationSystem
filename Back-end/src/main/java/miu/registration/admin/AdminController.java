e716497b62c5695b07099bd4e030cf598455c315
david <david.davaatulga@gmail.com>
2024-03-17 12:00:56 -0500

    Backend Development

--- Back-end/src/main/java/miu/registration/admin/AdminController.java
+++ Back-end/src/main/java/miu/registration/admin/AdminController.java
@@ -1,8 +1,10 @@
 package miu.registration.admin;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,20 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Admin APIs")
 @RestController
 @RequestMapping("admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final AdminService adminService;
+
+    @Operation(summary = "Process the registrations", description = "This API is used to process all the registrations.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registrations processed successfully")
+    })
     @PostMapping("process-registrations")
-    @Operation(summary = "Process the registrations")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<?> processRegistrations() {
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping("reset-registrations")
-    @Operation(summary = "Reset the registrations - for test")
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public ResponseEntity<?> resetRegistrations() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok(adminService.processRegistrations());
     }
 
 }
--- Back-end/src/main/java/miu/registration/teacher/TeacherController.java
+++ Back-end/src/main/java/miu/registration/teacher/TeacherController.java
@@ -3,27 +3,32 @@ package miu.registration.teacher;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import miu.registration.course.BlockCourse;
+import lombok.RequiredArgsConstructor;
+import miu.registration.block.BlockCourse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Teacher API")
+@Tag(name = "Teacher APIs")
 @RestController
 @RequestMapping("teacher")
+@RequiredArgsConstructor
 public class TeacherController {
 
     private final TeacherService teacherService;
 
-    public TeacherController(TeacherService teacherService) {
-        this.teacherService = teacherService;
+    @GetMapping
+    @Operation(summary = "Get list of all teachers")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public ResponseEntity<Iterable<Teacher>> getAllTeachers() {
+        return ResponseEntity.ok(teacherService.getTeachers());
     }
 
-    @GetMapping("{teacherName}")
+    @GetMapping("{teacherEmail}")
     @Operation(summary = "Get courses of the Teacher")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public ResponseEntity<List<BlockCourse>> getMyCourses(@PathVariable @Parameter(example = "Obinna Kalu") String teacherName) {
-        return ResponseEntity.ok(teacherService.getTeacherCourses(teacherName));
+    public ResponseEntity<List<BlockCourse>> getMyCourses(@PathVariable @Parameter(example = "kalu@miu.edu") String teacherEmail) {
+        return ResponseEntity.ok(teacherService.getTeacherCourses(teacherEmail));
     }
 }
